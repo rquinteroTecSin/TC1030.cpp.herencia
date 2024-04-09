5c352c116c1d3f7ff245eff5c6af98e734ca8d21
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2024-04-09 00:30:41 +0000

    Add Polygon class and update main function to create and print a Polygon object

---
+++ assignments/00-ShapeCircleRectanglePolygon/Polygon.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Shape.hpp"
+
+class Polygon : public Shape
+{
+public:
+    Polygon();
+    Polygon(int valX, int valY, int valSides);
+    int getSides();
+private:
+    int sides;
+};
